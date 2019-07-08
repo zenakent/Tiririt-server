bfc9c5363d68aecd82d50d265c56a9cb4f127d63
Maui <mauiandsonsph@yahoo.com>
2019-07-08 23:09:11 +0800

    fixed process.env.port to PORT

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 
 const db = require("./models");
 
-const PORT = process.env.port || 8081;
+const PORT = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(bodyParser.json());
